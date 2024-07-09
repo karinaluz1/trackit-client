d4a5bfd3a8aed79fe2aee77049bffc253bd5d097
Karina <karinaluz1@hotmail.com>
2024-07-09 13:48:56 -0300

    feat: add checkbox functionality

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,21 +1,30 @@
 import moment from 'moment';
 import Styles from './card.module.scss'
 import Button from '../button/button';
+import { useState } from 'react';
 
 function Card(props) {
+  const [done, setDone] = useState(false)
+
+  const handleCheckboxChange = (checked) => {
+    setDone(!checked)
+  }
+
   return (
-    <div className='card'>
-      <div className='card-data'>
-        <label className='card-label'>
-          <input type="checkbox" checked={props.complete} />
-          <div className="checkbox"></div>
-          <div className='card-title'>
+    <div className={Styles.card}>
+      <div className={Styles.data}>
+
+        <label className={Styles.label}>
+          <input type="checkbox" onChange={handleCheckboxChange} />
+          <div className={Styles.checkbox}></div>
+          <div className={Styles.title}>
             <span>{props.title}</span>
-            <span className='card-date'>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</span>
           </div>
         </label>
+
+        <span className={Styles.date}>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</span>
       </div>
-      <div className='card-buttons'>
+      <div className={Styles.buttons}>
       <Button content={<i className="fa-solid fa-pen"></i>} icon={true} type="secondary" />
         <Button content={<i className="fa-solid fa-trash-can"></i>} icon={true} type="error" />
       </div>
@@ -24,3 +33,14 @@ function Card(props) {
 }
 
 export default Card;
+
+
+
+
+
+
+
+
+
+
+
